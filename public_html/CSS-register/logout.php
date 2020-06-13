a076ae86f9c25446a6b04271edd38ba52908dfa9
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-06-13 17:27:59 -0400

    update logout

--- public_html/CSS-register/logout.php
+++ public_html/CSS-register/logout.php
@@ -1,8 +1,11 @@
 <?php
 include("header.php");
+echo "You have been logged out";
 session_unset();
 session_destroy();
-echo "You have been logged out";
-header( 'Location: login.php' ) ;
-
 ?>
+<!DOCTYPE html>
+<html>
+<h1>HTML Links</h1>
+<p><a href="login.php">Login</a></p>
+</html>
