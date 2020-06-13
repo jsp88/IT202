378db5cbc0ac04020b2f80ebb5f1c4983d6481bc
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-06-13 17:07:35 -0400

    updates CSS

--- public_html/CSS-register/header.php
+++ public_html/CSS-register/header.php
@@ -1,25 +1,45 @@
 
 <head>
-    <title>My site</title>
+    <title>Survey sit</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <?php
 require("config.php");
 session_start();
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+</style>
+</head>
+<body>
 <nav>
     <ul>
-        <li>
-            <a href="home.php">Home</a>
-        </li>
-        <li>
-            <a href="login.php">Login</a>
-        </li>
-        <li>
-            <a href="register.php">Register</a>
-        </li>
-        <li>
-            <a href="logout.php">Logout</a>
-        </li>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="logout.php">Logout</a></li>
     </ul>
 </nav>
+</body>
+</html>
