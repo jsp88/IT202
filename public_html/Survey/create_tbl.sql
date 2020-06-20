5f85ae7786dc3bcb2ded3bc8509539fd84f71420
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-06-20 14:17:48 -0400

    update survey

--- public_html/Survey/create_tbl.sql
+++ public_html/Survey/create_tbl.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Survey_Table  (
-    ID int NOT NULL,
-    Email varchar(254) ,
-    TodayDate date,
-    AnswerTime time,
-    PRIMARY KEY (ID)
-);
+CREATE TABLE Survey(
+    id int auto_increment,
+    title varchar(30) not null unique,
+    description TEXT,
+    visibility int, --DRAFT 0, Private 1, Publish 2
+    primary key (id)
+)
---
+++ public_html/Survey/edit.php
@@ -0,0 +1,73 @@
+<?php
+require("config.php");
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+$db = new PDO($connection_string, $dbuser, $dbpass);
+$Id = -1;
+$result = array();
+function get($arr, $key){
+    if(isset($arr[$key])){
+        return $arr[$key];
+    }
+    return "";
+}
+if(isset($_GET["Id"])){
+    $Id = $_GET["Id"];
+    $stmt = $db->prepare("SELECT * FROM Survey where id = :id");
+    $stmt->execute([":id"=>$Id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+else{
+    echo "No Id provided in url, don't forget this or sample won't work.";
+}
+?>
+
+<form method="POST">
+    <label for="title">Title
+	<input type="text" id="title" name="title" value="<?php echo get($result, "title");?>"/>
+	</label>
+	<label for="description">Description
+	<input type="text" id="description" name="description" value="<?php echo get($result, "description");?>"/>
+	</label>
+	<label for="visibility">Visibility
+	<input type="number" id="visibility" name="visibility" value="<?php echo get($result, "visibility");?>" />
+	</label>
+	<input type="submit" name="updated" value="Update Survey"/>
+</form>
+
+<?php
+if(isset($_POST["updated"])){
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $visibility = $_POST["visibility"];
+    if(!empty($title) && !empty($description) && !empty($visibility)){
+        try{
+            $stmt = $db->prepare("UPDATE Survey set title = :title, description=:description, visibility=:visibility where id=:id");
+            $result = $stmt->execute(array(
+                ":title" => $title,
+                ":description" => $description,
+                ":visibility" => $visibility,
+                ":id" => $Id
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
+            }
+            else{
+                echo var_export($result, true);
+                if ($result){
+                    echo "Successfully updated data: " . $title;
+                }
+                else{
+                    echo "Error updating record";
+                }
+            }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    else{
+        echo "Name and quantity must not be empty.";
+    }
+}
+?>
