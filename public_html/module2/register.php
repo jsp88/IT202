629b3c122fa80478dbe2f8a67f5bf8091b254ddd
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-07-24 22:18:01 -0400

    upload modlue2

--- public_html/module2/register.php
+++ public_html/module2/register.php
@@ -1,37 +1,6 @@
 <?php
 include("header.php");
 ?>
-<h1> Register here. </h1>
-<div>
-<span class="error">* required field</span>
-<form method="POST">
-    <label for="firstname">firstname
-    <input type="firstname" id="firstname" name="firstname" autocomplete="off" />
-    <span class="error" id="firstname">* <?php echo $fnameErr;?></span>
-    </label>
-    <label for="lastname">lastname
-    <input type="lastname" id="lastname" name="lastname" autocomplete="off" />
-    <span class="error" id="lastname">* <?php echo $LnameErr;?></span>
-    </label>
-        
-	<label for="email">Email
-	<input type="email" id="email" name="email" />
-	<span class="error" id="email">* <?php echo $emailError; ?></span>
-	</label><br><br>
-
-	<label for="p">Password
-	<input type="password" id="p" name="password" autocomplete="off"/>
-    <span class="error" id="p">* <?php echo $passErr;?></span>
-    </label><br><br>
-
-	<label for="cp">Confirm Password
-	<input type="password" id="cp" name="cpassword"/>
-    <span class="error" id="p">* <?php echo $passErr1;?></span>
-	</label><br><br>
-
-	<input type="submit" name="register" value="Register"/>
-</form>
-
 <?php
 $fnameErr=$LnameErr= "";
 $firstname=$lastname= "";
@@ -79,7 +48,7 @@ if(isset($_POST["register"])){
 			
 		}
 		
-	if(!empty($email) && !empty($password) && !empty($_POST["cpassword"]) !empty($firstname) !empty($lastname)){
+	if(!empty($email) && !empty($password) && !empty($_POST["cpassword"]) && !empty($firstname) && !empty($lastname)){
 		if($password == $cpassword){
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 			try{
@@ -110,3 +79,33 @@ if(isset($_POST["register"])){
 	}
 }
 ?>
+<h1> Register here. </h1>
+<div>
+<span class="error">* required field</span>
+<form method="POST">
+    <label for="firstname">firstname
+    <input type="firstname" id="firstname" name="firstname" autocomplete="off" />
+    <span class="error" id="firstname">* <?php echo $fnameErr;?></span>
+    </label>
+    <label for="lastname">lastname
+    <input type="lastname" id="lastname" name="lastname" autocomplete="off" />
+    <span class="error" id="lastname">* <?php echo $LnameErr;?></span>
+    </label>
+        
+	<label for="email">Email
+	<input type="email" id="email" name="email" />
+	<span class="error" id="email">* <?php echo $emailError; ?></span>
+	</label><br><br>
+
+	<label for="p">Password
+	<input type="password" id="p" name="password" autocomplete="off"/>
+    <span class="error" id="p">* <?php echo $passErr;?></span>
+    </label><br><br>
+
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword"/>
+    <span class="error" id="p">* <?php echo $passErr1;?></span>
+	</label><br><br>
+
+	<input type="submit" name="register" value="Register"/>
+</form>
