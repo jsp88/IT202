3fba978d1ae13d8655a2660ea4c40e03466c7260
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-07-25 10:43:53 -0400

    upload modlue2

--- public_html/module2/register.php
+++ public_html/module2/register.php
@@ -1,54 +1,81 @@
 <?php
 include("header.php");
 ?>
+<h1> Register here. </h1>
+<div>
+<span class="error">* REQUIRE FIELDS</span>
+<form method="POST">
+    <label for="firstname">firstname
+    <input type="firstname" id="firstname" name="firstname" autocomplete="off" />
+    <span class="error" id="firstname">* <?php echo $fnameErr;?></span>
+    </label> <br><br>
+    
+    <label for="lastname">lastname
+    <input type="lastname" id="lastname" name="lastname" autocomplete="off" />
+    <span class="error" id="lastname">* <?php echo $LnameErr;?></span>
+    </label><br><br>
+        
+	<label for="email">Email
+	<input type="email" id="email" name="email" />
+	<span class="error" id="email">* <?php echo $emailError; ?></span>
+	</label><br><br>
+
+	<label for="p">Password
+	<input type="password" id="p" name="password" autocomplete="off"/>
+    <span class="error" id="p">* <?php echo $passErr;?></span>
+    </label><br><br>
+
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword"/>
+    <span class="error" id="p">* <?php echo $passErr1;?></span>
+	</label><br><br>
+
+	<input type="submit" name="register" value="Register"/>
+</form>
 <?php
 $fnameErr=$LnameErr= "";
 $firstname=$lastname= "";
 $emailError = $passErr = "";
 $email = $password = "";
-
 if(isset($_POST["register"])){
-    if(empty($_POST["firstname"])){
-			
-			$fnameErr = "First name required";
-			
-			
-		} else {
+    if(empty($_POST["firstname"])
+       {
+        $fnameErr = "First name required";	
+		} 
+       else {
 			$firstname = $_POST["firstname"];
 		}
     if(empty($_POST["lastname"])){
-			
-			$LnameErr = "Last name required";
-			
-			
-		} else {
+			$LnameErr = "Last name required";	
+		} 
+       else {
 			$email = $_POST["email"];
 		} 
 	
-	if(empty($_POST["email"])){
-			
-			$emailError = "Email required";
-			
-			
-		} else {
+    if(empty($_POST["email"])){
+			$emailError = "Email required";		
+		} 
+       else {
 			$email = $_POST["email"];
 		}
-		if(empty($_POST["password"])){
+    if(empty($_POST["password"])){
 			
 			$passErr = "Password required";
-		} else {
+		} 
+       else {
 			$password = $_POST["password"];
 			
 		}
-		if(empty($_POST["cpassword"])){
+    if(empty($_POST["cpassword"])){
 			
 			$passErr1 = "Password confirmation required";
-		} else {
+		} 
+       else {
 			$cpassword = $_POST["cpassword"];
 			
 		}
 		
-	if(!empty($email) && !empty($password) && !empty($_POST["cpassword"]) && !empty($firstname) && !empty($lastname)){
+	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"]) && isset($_POST["firstname"] && isset($_POST["lastname"]){
 		if($password == $cpassword){
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 			try{
@@ -79,34 +106,3 @@ if(isset($_POST["register"])){
 	}
 }
 ?>
-<h1> Register here. </h1>
-<div>
-<span class="error">* required field</span>
-<form method="POST">
-    <label for="firstname">firstname
-    <input type="firstname" id="firstname" name="firstname" autocomplete="off" />
-    <span class="error" id="firstname">* <?php echo $fnameErr;?></span>
-    </label> <br><br>
-    
-    <label for="lastname">lastname
-    <input type="lastname" id="lastname" name="lastname" autocomplete="off" />
-    <span class="error" id="lastname">* <?php echo $LnameErr;?></span>
-    </label><br><br>
-        
-	<label for="email">Email
-	<input type="email" id="email" name="email" />
-	<span class="error" id="email">* <?php echo $emailError; ?></span>
-	</label><br><br>
-
-	<label for="p">Password
-	<input type="password" id="p" name="password" autocomplete="off"/>
-    <span class="error" id="p">* <?php echo $passErr;?></span>
-    </label><br><br>
-
-	<label for="cp">Confirm Password
-	<input type="password" id="cp" name="cpassword"/>
-    <span class="error" id="p">* <?php echo $passErr1;?></span>
-	</label><br><br>
-
-	<input type="submit" name="register" value="Register"/>
-</form>
