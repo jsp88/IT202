2d859b80eb1c35bbf56d659630525764834d7b81
jsp88 <65632273+jsp88@users.noreply.github.com>
2020-07-25 18:38:09 -0400

    upload modlue2

--- public_html/module2/survey.php
+++ public_html/module2/survey.php
@@ -1,68 +1,33 @@
 
 
-
-
-
 <?php
 
-
-
-include("header.php");
-
-	//$response = file_get_contents('php://input');
-	//$send = json_decode($response,true);
+require("config.php");
+	$response = file_get_contents('php://input');
+	$send = json_decode($response,true);
 	
-	//echo $field;
-
+	
 		$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
            	
 			$db = new PDO($connection_string, $dbuser, $dbpass);
 		
-            $sql = "SELECT Title FROM Survey";
-            //$stmt = $db->query($sql);
-			
-			
- 
- 
-
-			
+            $sql = "SELECT title FROM Survey";
 			
 		if ( $stmt = $db->query($sql)) {
 			while ($row = $stmt->fetch(PDO::FETCH_NUM)) {
-			printf ("%s \n", $row[0]) ;
-			
+				$term = $row[0];
+				$sendarray[] = array("titlename" => $term);
 			}
 			}
 		 $e = $stmt->errorInfo();
-		
+		$f = json_encode($sendarray);
+		echo $f;
 		}
         catch (Exception $e){
             echo $e->getMessage();
         }
 
-				 
-echo "bitch";
-echo "bitch";
-echo "bitch";
-echo "bitch";
-echo "bitch";
-echo "bitch";
 
 
-
-/*	
-$curl = curl_init();
-$url="https://web.njit.edu/~smg56/CS490/beta/ExamList.php";
-  
-  curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_POST => 1,
-    CURLOPT_FOLLOWLOCATION => 1,
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_POSTFIELDS => $field
-  )); 
-$resp = curl_exec($curl); 
-echo $resp;
-*/
 ?> 
